e3f60f6fb1a6b6f8fdb56562af8e93590c34c4cd
Mike Garton <cleric.of.serendipity@gmail.com>
2025-11-12 15:38:46 -0500

    re-organized server.ts, and fixed addgoals / addtask endpoints. Working on query endpoints

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ const app = express()
 const port = process.env.PORT || 8008; 
 app.use(express.json());
 
+
+//these are mostly testing, feel like they'll be obsolete eventually
 app.get('api/dbhealth', async (req, res) => {
     try {
         const result = await pool.query('SELECT NOW()');
@@ -27,134 +29,11 @@ app.get('api/health', (req, res) => {
   res.json({ status: 'ok', timestamp: new Date().toISOString() });
 });
 
-app.get('/api/pullusers', async (req, res) => {
-  try {
-    const results = await pool.query('SELECT * FROM goaltracker.users');
-    res.status(200).json(results.rows);
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
-});
+//might repurpose this one for a scoreboard want to modify for more complex queries for boards
 
-app.get('/api/getgoals', async (req,res) => {
-    const { userid } = req.body
-    try {
-        if (!userid) {
-            res.status(404).json({error: "User not found"})
-        }
 
-        const result = await pool.query(
-            "SELECT id, userid, title, description, status FROM goaltracker.goals WHERE userid = $1",
-            [userid]
-        )
-        if (result.rows.length === 0) {
-            res.status(404).json({error: "No goals ere M'lord"})
-        } else {
-            res.status(200).json(result);
-        }
-    } catch {
-        res.status(500).json({message: "No idea bruv, is busted"})
-    }
-});
 
-app.get('/api/gettasks', async (req, res) => {
-    try {
-        const { goalid } = req.body;
-        
-        if (!goalid) {
-            res.status(404).json({error: "Goal not found"})
-        }
-
-        const result = await pool.query(
-        "SELECT id, title, description, due_date FROM goaltracker.tasks WHERE goalid = $1",
-        [goalid]
-        );
-
-        if (result.rows.length === 0){
-            res.status(404).json({error: "No tasks ere M'lord"})
-        } else {
-        res.status(200).json(result)
-        }
-
-    } catch {
-        res.status(500).json({message: "issue with request"});
-    }
-})
-
-app.post('/api/adduser', async (req, res) => {
-    
-    try {
-        const { email, username, password} = req.body;
-
-        if (!email || !username || !password) {
-            return res.status(400).json({ error: "Missing required fields"});
-        }
-
-        const password_hash = await bcrypt.hash(password, 10);
-
-        const result = await pool.query(
-            'INSERT INTO goaltracker.users(email, username, password_hash) VALUES ($1, $2, $3) RETURNING *',
-            [email, username, password_hash]
-            );
-    
-        const newUser = result.rows[0];
-        delete newUser.password_hash;
-
-        res.status(200).json(newUser);
-    
-    } catch (error) {
-        console.error('Not letting this guy in cause:', error)
-        res.status(500).json({ error: "no can do chief" });
-    }
-});
-
-app.post('/api/addtask', async (req, res) =>{
-    try {
-        const { userid, title, description} = req.body
-
-        if (!userid || !title || !description) {
-            res.status(500).json(error: "Missing required fields")
-        }
-
-        const result = await pool.query(
-            "INSERT INTO goaltracker.goals (userid, title, description) VALUES ($1 ,$2, $3) RETURING *",
-            [userid, title, description]
-        );
-
-        const newGoal = result.rows[0];
-
-        res.status(200).json(newGoal);
-
-    } catch (error) {
-        console.error('It appears the scribes were asleep:', error)
-        res.status(500).json({ error: "caught the scribes napping"})
-    }
-});
-
-app.post('/api/addgoal', async (req, res) => {
-    const { goalid, title, description, due_date } = req.body;
-
-    try {
-        if (!goalid || !title || !description || !due_date) {
-            res.status(500).json({error: "missing required fields"})
-        }
-
-        const result = await pool.query(
-            "INSERT INTO goaltracker.tasks (goalid, title, description, due_date) VALUES ($1, $2, $3, $4) RETURNING *",
-            [goalid, title, description, due_date]
-        )
-
-        const newTask = result.rows[0];
-        res.status(200).json(newTask);
-
-    } catch (error) {
-        console.error("Error has occured: ", error)
-        res.status(500).json({ error: "Caught the scribes sleeeping" })
-    }
-});
-
-//pausing on this for now
+//USER MANAGEMENT APIs
 app.post('/api/login', async (req, res) => {
     const {username, password} = req.body
 
@@ -187,6 +66,142 @@ app.post('/api/login', async (req, res) => {
     }
 });
 
+app.post('/api/users/adduser', async (req, res) => {
+    
+    try {
+        const { email, username, password} = req.body;
+
+        if (!email || !username || !password) {
+            return res.status(400).json({ error: "Missing required fields"});
+        }
+
+        const password_hash = await bcrypt.hash(password, 10);
+
+        const result = await pool.query(
+            'INSERT INTO goaltracker.users(email, username, password_hash) VALUES ($1, $2, $3) RETURNING *',
+            [email, username, password_hash]
+            );
+    
+        const newUser = result.rows[0];
+        delete newUser.password_hash;
+
+        res.status(200).json(newUser);
+    
+    } catch (error) {
+        console.error('Not letting this guy in cause:', error)
+        res.status(500).json({ error: "no can do chief" });
+    }
+});
+
+app.get('/api/users/pullusers', async (req, res) => {
+  try {
+    const results = await pool.query('SELECT * FROM goaltracker.users');
+    res.status(200).json(results.rows);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+//GOAL INTERACT APIs
+app.get('/api/goals/getgoals/:user_id', async (req,res) => {
+    try {
+        const { user_id } = req.params;
+
+        if (!user_id) {    
+            res.status(400).json({error: "Must provide a username"});
+        }
+
+        const result = await pool.query(
+            "SELECT id, userid, title, description, status FROM goaltracker.goals WHERE userid = $1 RETURNING *",
+            [user_id]
+        )
+        if (result.rows.length === 0) {
+            res.status(404).json({error: "No goals ere M'lord"})
+        } else {
+            const goals = result.rows;
+            res.status(200).json(goals);
+        }
+    } catch {
+        res.status(500).json({message: "No idea bruv, is busted"})
+    }
+});
+
+app.post('/api/goals/addgoal', async (req, res) =>{
+    try {
+        const { user_id, title, description} = req.body
+
+        if (!user_id || !title || !description) {
+            res.status(500).json({error: "Missing required fields"})
+        }
+
+        const result = await pool.query(
+            "INSERT INTO goaltracker.goals (user_id, title, description) VALUES ($1 ,$2, $3) RETURNING *",
+            [user_id, title, description]
+        );
+
+        const newGoal = result.rows[0];
+
+        res.status(200).json(newGoal);
+
+    } catch (error) {
+        console.error('It appears the scribes were asleep:', error)
+        res.status(500).json({ error: "caught the scribes napping"})
+    }
+});
+
+
+//TASK INTERACT APIs
+app.get('/api/tasks/gettasks/:goal_id', async (req, res) => {
+    try {
+        const { goal_id } = req.params;
+        
+        if (!goal_id) {
+            res.status(404).json({error: "Goal not found"})
+        }
+
+        const result = await pool.query(
+        "SELECT id, title, description, due_date FROM goaltracker.tasks WHERE goalid = $1",
+        [goal_id]
+        );
+
+        if (result.rows.length === 0){
+            res.status(404).json({error: "No tasks ere M'lord"})
+        } else {
+        res.status(200).json(result.rows)
+        }
+
+    } catch {
+        res.status(500).json({message: "issue with request"});
+    }
+})
+
+app.post('/api/tasks/addtask/', async (req, res) => {
+    const { goal_id, title, description, due_date } = req.body;
+
+    try {
+        if (!goal_id || !title || !description || !due_date) {
+            res.status(500).json({error: "missing required fields"})
+        }
+
+        const result = await pool.query(
+            "INSERT INTO goaltracker.tasks (goal_id, title, description, due_date) VALUES ($1, $2, $3, $4) RETURNING *",
+            [goal_id, title, description, due_date]
+        )
+
+        const newTask = result.rows[0];
+        res.status(200).json(newTask);
+
+    } catch (error) {
+        console.error("Error has occured: ", error)
+        res.status(500).json({ error: "Caught the scribes sleeeping" })
+    }
+});
+
+app.put("/api/tasks/modify", async (req, res) => {
+    console.log("not built yet")
+})
+
+//LISTENING POSTS
 app.listen(port, () => {
     console.log(`Winter Arc is Go on ${port}`);
 });
